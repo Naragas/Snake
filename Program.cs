3b77f43f6264f6b9cbc7e5ae33759cf29497476e
Naragas <naragas@gmail.com>
2019-06-28 13:27:57 +0300

    Процедурный подход.

--- Program.cs
+++ Program.cs
@@ -10,9 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
 
+            PrintSymbol(x1, y1, sym1);
+            PrintSymbol(x2, y2, sym2);
+            Console.ReadKey();
+
+        }
+
+        private static void PrintSymbol(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
         }
     }
 }
