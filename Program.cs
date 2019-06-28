2213532b1be35c649594ba157ecd30d987140865
Naragas <naragas@gmail.com>
2019-06-28 15:54:20 +0300

    Инкапсуляция

--- Program.cs
+++ Program.cs
@@ -10,20 +10,12 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            //int x1 = 1;
-            //int y1 = 3;
-            //char sym1 = '*';
-
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.symbol = '*';
+            Point p1 = new Point(1,3,'*');
+            p1.PrintSymbol();
 
             Point p2 = new Point(4, 5, '#');
-
             p2.PrintSymbol();
-            Console.ReadKey();
-            p1.PrintSymbol();
+           
             Console.ReadKey();
         }
 
