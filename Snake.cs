9c6ab579ca0b21b0e5a9db21add5537461a96aef
Naragas <naragas@gmail.com>
2019-06-29 01:49:21 +0300

    SnakeControle

--- Snake.cs
+++ Snake.cs
@@ -11,17 +11,19 @@ namespace Snake
         Direction direction;
         public Snake(Point tail, int length, Direction _direction)
         {
-            direction = _direction;
+            Direction = _direction;
             pList = new List<Point>();
 
             for (int i = 0; i < length; i++)
             {
                 Point p = new Point(tail);
-                p.Move(i, direction);
+                p.Move(i, Direction);
                 pList.Add(p);
             }
 
         }
+
+        internal Direction Direction { get => direction; set => direction = value; }
 
         internal void Move()
         {
@@ -32,15 +34,27 @@ namespace Snake
 
             tail.Clear();
             head.Draw();
-            
+
         }
 
         private Point GetNextPoint()
         {
             Point head = pList.Last();
             Point nextPoint = new Point(head);
-            nextPoint.Move(1, direction);
+            nextPoint.Move(1, Direction);
             return nextPoint;
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
         }
     }
 }
